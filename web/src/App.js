0110cad186c2b71faefd50bf0622915d14f7913f
Brandon Davis <dbrandon.91@gmail.com>
2024-04-22 20:02:32 -0700

    Add dark theme

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,9 @@ import Alert from "@mui/material/Alert";
 import LoadingButton from "@mui/lab/LoadingButton";
 import WifiFindIcon from "@mui/icons-material/WifiFind";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import Grid from "@mui/material/Grid";
 
 import WifiStatus from "./components/WifiStatus";
@@ -11,6 +14,15 @@ import VehicleStatus from "./components/VehicleStatus";
 
 import API from "./api/twc.js";
 import VitalsChart from "./components/VitalsChart.js";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#d0d1d2",
+    },
+  },
+});
 
 function App() {
   const [isConnected, setConnected] = useState(false);
@@ -37,41 +49,44 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Grid container spacing={{ xs: 2, md: 3 }}>
-        <Grid item xs={12}>
-          <Alert severity="info">
-            Tesla Wall Connector Gen 3 is only supported!
-          </Alert>
-          {isServerError && (
-            <Alert severity="warning">Server disconnected!</Alert>
-          )}
-          {!isConnected && (
-            <LoadingButton
-              onClick={findTWC}
-              loading={isFindingTWC}
-              loadingPosition="start"
-              startIcon={<WifiFindIcon></WifiFindIcon>}
-              variant="outlined"
-            >
-              Find
-            </LoadingButton>
-          )}
-        </Grid>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          <Grid item xs={12}>
+            <Alert severity="info">
+              Tesla Wall Connector Gen 3 is only supported!
+            </Alert>
+            {isServerError && (
+              <Alert severity="warning">Server disconnected!</Alert>
+            )}
+            {!isConnected && (
+              <LoadingButton
+                onClick={findTWC}
+                loading={isFindingTWC}
+                loadingPosition="start"
+                startIcon={<WifiFindIcon></WifiFindIcon>}
+                variant="outlined"
+              >
+                Find
+              </LoadingButton>
+            )}
+          </Grid>
 
-        <Grid item xs={6}>
-          {isConnected && <WifiStatus></WifiStatus>}
-        </Grid>
+          <Grid item xs={6}>
+            {isConnected && <WifiStatus></WifiStatus>}
+          </Grid>
 
-        <Grid item xs={6}>
-          {isConnected && <VehicleStatus></VehicleStatus>}
-        </Grid>
+          <Grid item xs={6}>
+            {isConnected && <VehicleStatus></VehicleStatus>}
+          </Grid>
 
-        <Grid item xs={12}>
-          {isConnected && <VitalsChart></VitalsChart>}
+          <Grid item xs={12}>
+            {isConnected && <VitalsChart></VitalsChart>}
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
